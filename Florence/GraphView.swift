86f8e4be97e3d95028b897b56a76d969305a6795
Lujul <lujul@air-de-lujul>
2017-03-16 11:11:21 +0100

    debut graph view flip flip

---
+++ Florence/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Florence
+//
+//  Created by Lujul on 16/03/2017.
+//  Copyright © 2017 lujul. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Florence/ViewController.swift
+++ Florence/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Counter outlets
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        counterLabel.text = String(counterView.counter)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnPushButton(button: PushButtonView) {
+        if button.isAddButton {
+            counterView.counter += 1
+        } else {
+            if counterView.counter > 0 {
+                counterView.counter -= 1
+            }
+        }
+        counterLabel.text = String(counterView.counter)
+    }
+    
 
 
 }
